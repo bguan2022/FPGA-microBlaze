14c6b23881dfe30bd2669cfe1e82924b44f1990b
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-12-05 11:34:11 -0800

    Update readSPI_demo.c

--- readSPI_demo.c
+++ readSPI_demo.c
@@ -1,9 +1,25 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "sleep.h"
 
+#include "xparameters.h"
+#include "xiic.h"
+#include "xil_io.h"
+
+#include "cli.h"
+#include "i2c.h"
 
 
 int main(){
   u32 spi_addr,offset_addr,*readDat;
-  readSPIReg(spi_addr,offset_addr,*readData);
-
+  sou_addr = 0x44000000;
+  offset_addr = 0;
+  
+  while (1){
+    readSPIReg(spi_addr,offset_addr,*readData);
+    sleep(1000); //pause 1 sec
+  }
 }; 
