00060ea638cc6d87ca73a2d6d4e42b614411fb95
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-12-05 11:01:35 -0800

    Update spi.h

--- headers/spi.h
+++ headers/spi.h
@@ -1,12 +1,12 @@
 #ifndef SPI_H
 #define SPI_H
 
-#define SPI_DEVICE_ID XPAR_AXI_IIC_0_DEVICE_ID
-#define SPI_IIC_ADDR XPAR_IIC_0_BASEADDR
+#define SPI_DEVICE_ID XPAR_AXI_SPI_0_DEVICE_ID
+#define SPI_SPI_ADDR XPAR_IIC_SPI_BASEADDR
 #define SPI_WRITE_LEN_MAX 128
 
 
 int readSPIReg(u32 spi_addr, u32 offset, u32 *ReadData);
-int writeSPIReg(u8 spi_addr, u32 offset, u32 writeData);
+int writeSPIReg(u32 spi_addr, u32 offset, u32 writeData);
 
 #endif
