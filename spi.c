1bef223b18978d690be83519cc9a4d0321ffca92
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-12-05 14:49:08 -0800

    Update spi.c

--- spi.c
+++ spi.c
@@ -16,19 +16,19 @@ int check_spi_status(stat){
 	     	printf ("if the device is started. It must be stopped to re-initialize.");
 	else if  (stat == XST_DEVICE_NOT_FOUND ) 
 	      	printf ("if the device was not found in the configuration such that initialization could not be accomplished."); 
-}
+};
 
 int readSPIReg(u32 SPI_ADDR, u32 offset, u32 *readData) {
 	*readData=XSpi_ReadReg(SPI_ADDR, spi_addr);
 	printf("Read %d SPI reg at %d, value is", writeData, SPI_ADDR+offset, *readData);
 	return inBuffer;
-}
+};
 
 int writeSPIReg(u32 SPI_ADDR, u32 offset, u32 writeData) {
 	XSpi_WriteReg(SPI_ADDR, offset, writeData, addrSize);
 	printf("Writing %d to SPI reg at %d", writeData, SPI_ADDR+offset);
 	return 0;
-}
+};
 
 
 
