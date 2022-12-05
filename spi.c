94d86e899f88619ed2d4e621aa51b8a2794c07e8
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-12-05 14:28:42 -0800

    Update spi.c

--- spi.c
+++ spi.c
@@ -9,6 +9,14 @@
 #define WR_DATA_LEN 			(WR_LEN_MAX + 2)
 #define RD_LEN_MAX				128
 
+int check_spi_status(stat){
+	if (stat == XST_SUCCESS) 
+      		printf ("SPI initialization is successful.");
+	else if (stat == XST_DEVICE_IS_STARTED)
+	     	printf ("if the device is started. It must be stopped to re-initialize.");
+	else if  (stat == XST_DEVICE_NOT_FOUND ) 
+	      	printf ("if the device was not found in the configuration such that initialization could not be accomplished."); 
+}
 
 int readSPIReg(u32 SPI_ADDR, u32 offset, u32 *readData) {
 	*readData=XSpi_ReadReg(SPI_ADDR, spi_addr);
